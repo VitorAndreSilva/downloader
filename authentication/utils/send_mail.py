63b6d9795793233f052713d573f05073585dfa96
VitorAndreSilva <alunovitorandre@gmail.com>
2026-01-31 14:49:58 -0300

    FEAT: Adiciona template HTML para envio de emails de autenticação

--- authentication/utils/send_mail.py
+++ authentication/utils/send_mail.py
@@ -1,14 +1,28 @@
+import os
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from django.utils.html import strip_tags
 from django.conf import settings
+from config.settings import BASE_DIR
 
 def registration_request(user):
     admins = User.objects.filter(is_staff=True, is_active=True)
     emails = [admin.email for admin in admins if admin.email]
 
-    send_mail(
-        subject="Novo usuário aguardando aprovação",
-        message=f"Novo cadastro: {user.email}",
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=emails,
-    )
+    try:
+        with open(os.path.join(BASE_DIR, "authentication/templates/email.html"), "r", encoding="utf-8") as file:
+            html = file.read()
+            html = html.replace("{{email}}", user.email)
+
+        message = strip_tags(html)
+
+        send_mail(
+            subject="Novo usuário aguardando aprovação",
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=emails,
+            html_message=html
+        )
+
+    except Exception as e:
+        print("Falha ao enviar email: ", e)
